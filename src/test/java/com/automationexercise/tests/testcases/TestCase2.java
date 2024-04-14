f57305210b8e9086edb71ed040655ba0fdc1282e
Ahmet Beşkazalıoğlu <ahmetbeskazalioglu@icloud.com>
2024-04-14 21:07:45 +0300

    gfhfgh

---
+++ src/test/java/com/automationexercise/tests/testcases/TestCase2.java
@@ -0,0 +1,12 @@
+package com.automationexercise.tests.testcases;
+
+import com.automationexercise.tests.TestBase;
+import org.testng.annotations.Test;
+
+public class TestCase2 extends TestBase {
+
+    @Test
+    public void ahmet(){
+
+    }
+}
